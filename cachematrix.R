410a2e448c2ad8becf34d5ff4bcb35015c878093
cata <catalina.enescu@evomind.info>
2015-02-18 17:39:32 +0200

    test

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 
 ## makeCacheMatrix: This function creates a 
 ## special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
   set <- function(y) {
